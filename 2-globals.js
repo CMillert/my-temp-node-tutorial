5f244f541aefed88e7e3ef2c1917c11fa97e12ca
Millert83 <carl.millert@outlook.com>
2021-05-08 11:23:51 +0200

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// GLOBALS - NO WINDOW !!!!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use (CommonJS)
+// module - info about current module
+// process - info about env where the program is executed
+console.log(__filename);
+setInterval(()=> {
+console.log('hello world');
+}, 1000)
---
+++ 7.mind-grenade.js
@@ -0,0 +1,8 @@
+const numm1 = 5
+const numm2 = 10
+
+function addValues() {
+    console.log(`the sum is : ${numm1 + numm2}`)
+}
+
+addValues()
